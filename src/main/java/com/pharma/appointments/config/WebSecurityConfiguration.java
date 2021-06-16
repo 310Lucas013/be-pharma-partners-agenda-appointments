adcc4659dc4396b6c4de123b20666bf897b69b9b
JayvHelderen <jayvanhelderen@hotmail.com>
2021-06-16 14:21:37 +0200

    Update WebSecurityConfiguration.java

--- src/main/java/com/pharma/appointments/config/WebSecurityConfiguration.java
+++ src/main/java/com/pharma/appointments/config/WebSecurityConfiguration.java
@@ -23,8 +23,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
-                .authorizeRequests()
+        httpSecurity.authorizeRequests()
                 // all other requests need to be authenticated
                 .anyRequest().authenticated().and()
                 // make sure we use stateless session; session won't be used to
